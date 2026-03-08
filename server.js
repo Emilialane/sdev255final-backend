d9966f822f938ebf8b896ebcacbb5c919e2d5119
Emilialane <elane45@ivytech.edu>
2026-03-08 03:26:08 +0000

    Add fallback MongoDB URI

--- server.js
+++ server.js
@@ -26,3 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.log(err));
+  const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/sdev255';
+mongoose.connect(mongoUri)
